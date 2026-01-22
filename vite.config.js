ec265246fe0b8377a69c5b4a52c49f8a1b99d60a
Rajababu Kushwaha <kushwahr@roehampton.ac.uk>
2026-01-22 13:11:03 +0000

    Add proxy configuration to Vite dev server
    Configured the Vite development server to proxy '/api' and '/media' requests to 'http://87.106.96.173'. This allows local development to interact with the backend and media endpoints without CORS issues.

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,21 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://87.106.96.173',
+        changeOrigin: true,
+        secure: false,
+      },
+      '/media': {
+        target: 'http://87.106.96.173',
+        changeOrigin: true,
+        secure: false,
+      },
+    }
+  }
 })
